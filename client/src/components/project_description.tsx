9830208b67116322ed635f8d78bc9819d3515e8f
rlaclgh <rlaclgh11@gmail.com>
2024-02-15 16:12:50 +0900

    [client] 프로젝트 설명 수정

--- client/src/components/project_description.tsx
+++ client/src/components/project_description.tsx
@@ -4,9 +4,14 @@ const ProjectDescription = () => {
   return (
     <div className="flex items-center h-full flex-col">
       <div className="w-120">
-        <div className="text-5xl mb-10 mt-10">프로젝트 명</div>
+        <div className="text-5xl mb-10 mt-10">miniproject_image_upload</div>
         <div className="text-2xl my-2">1. 프로젝트 설명</div>
-        <div className="text-xl my-2">프로젝트 설명입니다.</div>
+        <div className="text-xl my-2">
+          1.1 client에서 image 업로드시 server에서 presigned url 발급
+        </div>
+        <div className="text-xl my-2">
+          1.2 client에서 presigned url 로 image s3에 업로드
+        </div>
       </div>
     </div>
   );
